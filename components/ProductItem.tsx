bf5d1972638702878ef4b34469fb9c2d1f4d3387
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-12-23 08:54:18 -0300

    🚀 CodeSpliting: perform next app using dynamic import component

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,21 +1,42 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
+
+import dynamic from 'next/dynamic'
+import { AddProductToWishListProps } from './AddProductToWishList'
+
+const AddProductToWishList = dynamic<AddProductToWishListProps>(() => {
+  return import('./AddProductToWishList').then(mod => mod.AddProductToWishList)
+}, {
+  loading: () => <span>Carregando...</span>
+})
 
 type ProductItemProps = {
   product: {
     id: number,
     price: number,
+    priceFormatted: string,
     title: string,
-  }
+  },
+  onAddToWishList: (id: number) => void,
 }
 
 // shallow compare -> comparação rasa 
 // {} === {} // false
 // igualdade referencial
 
-function ProductItemComponent({ product }: ProductItemProps) {
+function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+  const [isAddingToWishList, setIsAddingToWishList] = useState(false)
+
   return (
     <div>
-      {product.title} - <strong>{product.price}</strong>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => setIsAddingToWishList(true)}>Add to wishList</button>
+
+      {isAddingToWishList && (
+        <AddProductToWishList
+          onAddToWishList={() => onAddToWishList(product.id)}
+          onRequestClose={() => setIsAddingToWishList(false)}
+        />
+      )}
     </div>
   )
 }
