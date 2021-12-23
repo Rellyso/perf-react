225099e77d2b94191472e69f2a00e531e2b27585
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-12-23 08:51:23 -0300

    ℹ️ using data formatter before script rendering

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -5,22 +5,19 @@ type SearchResultsProps = {
   results: Array<{
     id: number,
     price: number,
+    priceFormatted: string,
     title: string,
-  }>
+  }>,
+  totalPrice: number,
+  onAddToWishList: (id: number) => void,
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
-  const totalPrice = useMemo(() => {
-    return results.reduce((total, product) => {
-      return total + product.price
-    }, 0)
-  }, [results])
-
+export function SearchResults({ results, onAddToWishList }: SearchResultsProps) {
   return (
     <div>
       {results?.map((product) => {
         return (
-          <ProductItem key={product.id} product={product} />
+          <ProductItem key={product.id} product={product} onAddToWishList={onAddToWishList} />
         )
       })}
     </div>
